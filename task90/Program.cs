f4c43d7d0718e579560de0d8eb9b56e830c68425
Peter <renemebel@yandex.ru>
2022-12-04 12:30:13 +0300

    вывести все нат числа от n до 1

---
+++ task90/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+*/
+
+
+void printRange(int index)
+{
+    if (index != 0 )
+    {
+             Console.Write(index + ",");
+             printRange(index - 1);
+    }
+} 
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+printRange(N);
